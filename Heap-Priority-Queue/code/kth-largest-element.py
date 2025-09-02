7dc63111904e5e1a242a6d26875b9da8dd3e8fe8
daishadavis <daishadavis84@gmail.com>
2025-09-02 16:05:56 -0400

    kth-largest code

---
+++ Heap-Priority-Queue/code/kth-largest-element.py
@@ -0,0 +1,19 @@
+import heapq
+from typing import List
+
+
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.minHeap = nums
+        heapq.heapify(self.minHeap)
+        while len(self.minHeap) > k:
+            heapq.heappop(self.minHeap)
+
+    def add(self, val: int) -> int:
+        """Adds a value to the min-heap then returns the kth largest number"""
+        heapq.heappush(self.minHeap, val)
+        if len(self.minHeap) > self.k:
+            heapq.heappop(self.minHeap)
+        return self.minHeap[0]
