131df0c7b9ef8bfb196f3a5e14301353f00a7fd9
daishadavis <daishadavis84@gmail.com>
2025-09-07 18:11:31 -0400

    add code templates

---
+++ Code Templates/two-pointers.py
@@ -0,0 +1,13 @@
+def fn(arr):
+    """Two Pointers: one input, opposite ends"""
+    left = ans = 0
+    right = len(arr) - 1
+    
+    while left < right:
+        # do some logic here with the left and right
+        if CONDITION:
+            left += 1
+        else:
+            right -= 1
+            
+    return ans
